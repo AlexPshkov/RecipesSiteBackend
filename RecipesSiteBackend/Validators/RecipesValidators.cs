3689a723be1401e105b9c1d5b86e6ab19e91c8b6
AlexPshkov <alexpshkov@gmail.com>
2022-09-17 14:01:15 +0300

    Remove minLength validator

--- RecipesSiteBackend/Validators/RecipesValidators.cs
+++ RecipesSiteBackend/Validators/RecipesValidators.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
 using RecipesSiteBackend.Exceptions;
+using RecipesSiteBackend.Exceptions.Implementation;
 using RecipesSiteBackend.Storage.Entities.Implementation;
 using RecipesSiteBackend.Storage.Entities.Implementation.secondary;
 
@@ -7,12 +8,11 @@ namespace RecipesSiteBackend.Validators;
 
 public static class RecipesValidators
 {
-    private const int MinStringLength = 3;
-    
+
     public static RecipeEntity ValidateRecipe( this RecipeEntity recipeEntity )
     {
-        if ( recipeEntity.RecipeName.IsNullOrEmpty() || recipeEntity.RecipeName.Length < MinStringLength ) throw new InvalidRecipeException( "RecipeName is empty", recipeEntity );
-        if ( recipeEntity.RecipeDescription.IsNullOrEmpty() || recipeEntity.RecipeDescription.Length < MinStringLength ) throw new InvalidRecipeException( "RecipeDescription is empty", recipeEntity );
+        if ( recipeEntity.RecipeName.IsNullOrEmpty() ) throw new InvalidRecipeException( "RecipeName is empty", recipeEntity );
+        if ( recipeEntity.RecipeDescription.IsNullOrEmpty() ) throw new InvalidRecipeException( "RecipeDescription is empty", recipeEntity );
 
         recipeEntity.Ingredients = recipeEntity.Ingredients.ValidateIngredients();
         recipeEntity.Steps = recipeEntity.Steps.ValidateSteps();
@@ -25,20 +25,16 @@ public static class RecipesValidators
     {
         return ingredientEntities.Where( entity => 
             !entity.Title.IsNullOrEmpty() &&
-            entity.Title.Length >= MinStringLength &&
-            !entity.Description.IsNullOrEmpty() &&
-            entity.Description.Length >= MinStringLength ).ToList();
+            !entity.Description.IsNullOrEmpty() ).ToList();
     }
     
     private static List<StepEntity>  ValidateSteps( this IEnumerable<StepEntity> stepEntities )
     {
-        return stepEntities.Where( entity => !entity.Description.IsNullOrEmpty() &&
-                                             entity.Description.Length >= MinStringLength ).ToList();
+        return stepEntities.Where( entity => !entity.Description.IsNullOrEmpty() ).ToList();
     }
 
     private static List<TagEntity>  ValidateTags( this IEnumerable<TagEntity> tagEntities )
     {
-        return tagEntities.Where( entity => !entity.Name.IsNullOrEmpty() &&
-                                            entity.Name.Length >= MinStringLength ).ToList();
+        return tagEntities.Where( entity => !entity.Name.IsNullOrEmpty() ).ToList();
     }
 }
